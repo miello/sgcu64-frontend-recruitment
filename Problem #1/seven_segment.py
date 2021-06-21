38d9089d648070ab2dbcb1339c32ebb3527af35b
miello <partearth1212@gmail.com>
2021-06-22 01:35:34 +0700

    📝 Write documentation for printSevenSegment

--- Problem #1/seven_segment.py
+++ Problem #1/seven_segment.py
@@ -37,25 +37,52 @@ def generateSegment(num, isInvalid = False):
         num //= 10
     return extractList[::-1]
 
-# The method for printing the seven segment
+''' 
+The method for printing the seven segment
+Input:
+    hours: the value of hours
+    minutes: the value of minutes
+    seconds: the value of seconds
+Return:
+    None
+'''
 def printSevenSegment(hours, minutes, seconds):
+    # Check the invalid case
     invalidTime = False
     if minutes > 59 or seconds > 59:
         invalidTime = True
 
+    # Generate the template list 
     hours = generateSegment(hours, invalidTime)
     minutes = generateSegment(minutes, invalidTime)
     seconds = generateSegment(seconds, invalidTime)
+
+    # Concatenate all list of segment in order hours -> minutes -> seconds
     allSegment = hours + minutes + seconds
+
+    # Iterate over each row which is three
     for i in range(3):
+        # Iterate over each element in allSegment with tracing index
         for idx, (row, column) in enumerate(allSegment):
+            # Iterate over each character in template but why plus four ?
+            # Because each seven segment have a width four and height three
             for columnPrint in range(column, column + 4):
                 print(segment_str[row + i][columnPrint], end='')
+
+            # print seperator if item is the odd element in allSegment and not the first and last element 
             if idx % 2 != 0 and idx != 5 and i != 0:
                 print(seperator, end='')
+
+            # print just a space width three when item is the odd element and is in the first row
             elif idx % 2 != 0 and i == 0:
                 print('   ', end='')
+
+            # print space width one 
             else: 
                 print(' ', end='')
+
+        # print new line
         print()
+
+
 hours, minutes, seconds = map(int, input().split(':'))
