8cbd25b535ae5fd76f9a97906a0d93bf76911989
miello <partearth1212@gmail.com>
2021-06-22 12:21:46 +0700

    ✨ Implement the Chula chana menu

--- Problem #2/chula_chana.py
+++ Problem #2/chula_chana.py
@@ -8,7 +8,27 @@ def checkInPage():
     pass
 
 def main():
-    print('main')
-    pass
+    while(True):
+        print('Welcome to Chula Chana!!!')
+        print('Available commands:')
+        print('  1. Check in user')
+        print('  2. Check out user')
+        print('  3. Print people count')
+        print('  4. Exit')
+        command = input('Please input any number: ')
+        try:
+            command = int(command)
+            if command == 1:
+                checkInPage()
+            elif command == 2:
+                checkOutPage()
+            elif command == 3:
+                peopleCountPage()
+            elif command == 4:
+                exit(0)
+            else:
+                print('Invalid number')
+        except:
+            print('Invalid type of input')
 
 main()
