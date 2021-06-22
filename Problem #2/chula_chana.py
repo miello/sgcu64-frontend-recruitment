81fd00cd7e8e78b865103834aba02daa145debdf
miello <partearth1212@gmail.com>
2021-06-22 13:57:24 +0700

    💡 Add comment to each line of code

--- Problem #2/chula_chana.py
+++ Problem #2/chula_chana.py
@@ -1,6 +1,15 @@
+# Initialize the constant variable and database
 menu_seperator = '-----------------------------------------------------------------'
+
+# Represent the list of available location
 locationList = ['Mahamakut Building', 'Sara Phra Kaew', 'CU Sport Complex', 'Sanum Juub', 'Samyan Mitr Town']
+
+# The database for storing number of people in location
+# [str] -> int
 peopleCountLocation = dict()
+
+# The database for mapping phone number to location
+# [str] -> str
 phoneLocation = dict()
 
 '''
@@ -24,6 +33,7 @@ Return:
 def peopleCountPage():
     print('Current Population')
     for idx, location in enumerate(locationList):
+        # Get number of people in location
         peopleCount = peopleCountLocation[location]
         print(f'  {idx + 1}. {location}: {peopleCount}')
 '''
@@ -38,16 +48,25 @@ def checkOutPage():
     print('Check out')
     phoneNumber = input('Enter phone number: ')
 
+    # If phone number key is not in phoneLocation 
+    # then add the new phone number
     if phoneNumber not in phoneLocation:
         phoneLocation[phoneNumber] = ''
     
     msg = None
+
+    # Case: Not check in
     if phoneLocation[phoneNumber] == '':
         msg = 'You have not checked in yet'
+
+    # Case: Check out from location
     else:
         oldPlace = phoneLocation[phoneNumber]
 
+        # Decrease people counter
         peopleCountLocation[oldPlace] -= 1
+
+        # Reset the location of phone number
         phoneLocation[phoneNumber] = ''
         
         msg = f'Check out from {oldPlace}'
@@ -64,12 +83,15 @@ Return:
 '''
 def checkInPage():
     print('Check in')
+    # Phone number input
     phoneNumber = input('Enter phone number: ')
     
     for idx, location in enumerate(locationList):
         print(f'  {idx + 1}. {location}')
 
     place = None
+
+    # Loop until get the valid index input
     while True:
         try: 
             place = input('Select the place: ')
@@ -80,23 +102,37 @@ def checkInPage():
         except:
             print('Invalid index of place. Please try again.')
     
+    # Get name of place by index
     nameOfPlace = locationList[place - 1]
     msg = ''
 
+    # If phone number key is not in phoneLocation 
+    # then add the new phone number
     if phoneNumber not in phoneLocation:
         phoneLocation[phoneNumber] = ''
     
+    # Case: Not check in yet
     if phoneLocation[phoneNumber] == '':
         phoneLocation[phoneNumber] = nameOfPlace
         peopleCountLocation[nameOfPlace] += 1
         msg = f'Check in at {nameOfPlace}'
+    
+    # Case: The location which will check in is equal to current check in location
     elif phoneLocation[phoneNumber] == nameOfPlace:
         msg = f'You have already checked in at {nameOfPlace}'
+    
+    # Case: The location which will check in is not equal to current check in location
     else:
+        # Get the current location
         oldPlace = phoneLocation[phoneNumber]
+
+        # Change to new location
         phoneLocation[phoneNumber] = nameOfPlace
 
+        # Decrease number of people of past location
         peopleCountLocation[oldPlace] -= 1
+
+        # Increase number of people of new location
         peopleCountLocation[nameOfPlace] += 1
 
         msg = f'Check out from {oldPlace} and check in at {nameOfPlace}'
@@ -111,7 +147,10 @@ Return:
     None
 '''
 def main():
+    # Initialize the database
     initializeDatabase()
+
+    # Loop the menu
     while(True):
         print('Welcome to Chula Chana!!!')
         print('Available commands:')
@@ -123,6 +162,8 @@ def main():
         command = input('Please input any number: ')
 
         print(menu_seperator)
+
+        # Check the error from parsing
         try:
             command = int(command)
         except:
@@ -143,4 +184,5 @@ def main():
             print('Invalid number')
         print(menu_seperator)
 
+# Run the main program
 main()
