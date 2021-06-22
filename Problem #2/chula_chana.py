029a093f71a2a6f091587db202a5f3943781e364
miello <partearth1212@gmail.com>
2021-06-22 12:38:01 +0700

    ✨ Implement the database

--- Problem #2/chula_chana.py
+++ Problem #2/chula_chana.py
@@ -1,3 +1,8 @@
+menu_seperator = '-----------------------------------------------------------------'
+locationList = ['Mahamakut Building', 'Sara Phra Kaew', 'CU Sport Complex', 'Sanum Juub', 'Samyan Mitr Town']
+peopleCountLocation = dict()
+phoneLocation = dict()
+
 def peopleCountPage():
     pass
 
@@ -8,6 +13,7 @@ def checkInPage():
     pass
 
 def main():
+    initializeDatabase()
     while(True):
         print('Welcome to Chula Chana!!!')
         print('Available commands:')
@@ -15,20 +21,31 @@ def main():
         print('  2. Check out user')
         print('  3. Print people count')
         print('  4. Exit')
+
         command = input('Please input any number: ')
+
+        print(menu_seperator)
         try:
             command = int(command)
-            if command == 1:
-                checkInPage()
-            elif command == 2:
-                checkOutPage()
-            elif command == 3:
-                peopleCountPage()
-            elif command == 4:
-                exit(0)
-            else:
-                print('Invalid number')
         except:
             print('Invalid type of input')
+            print(menu_seperator)
+            continue
+
+        if command == 1:
+            checkInPage()
+        elif command == 2:
+            checkOutPage()
+        elif command == 3:
+            peopleCountPage()
+        elif command == 4:
+            exit(0)
+        else:
+            print('Invalid number')
+        print(menu_seperator)
+
+def initializeDatabase():
+    for location in locationList:
+        peopleCountLocation[location] = 0
 
 main()
